6892b425b045c8e884bd865cdf74dc06cf35e5f4
SpinnyMemer <110652422+SpinnyMemer@users.noreply.github.com>
2025-01-06 21:31:17 +0530

    Create GetIslands.lua

---
+++ GetIslands.lua
@@ -0,0 +1,123 @@
+local Dummyspace = game.Workspace:WaitForChild("Dummyspace")
+
+local IslandPositions = Instance.new("Folder", Dummyspace)
+IslandPositions.Name = "IslandPositions"
+
+local Colosseum = Instance.new("Part", IslandPositions)
+Colosseum.Name = "Colosseum"
+Colosseum.Anchored = true
+Colosseum.CanCollide = false
+Colosseum.Transparency = 1
+Colosseum.CFrame = CFrame.new(-1332.94006, 7.33036804, -2896.56055, -0.66911, 0, 0.743163407, 0, 1, 0, -0.743163407, 0, -0.66911)
+
+local Desert = Instance.new("Part", IslandPositions)
+Desert.Name = "Desert"
+Desert.Anchored = true
+Desert.CanCollide = false
+Desert.Transparency = 1
+Desert.CFrame = CFrame.new(974.707397, 6.48023319, 4141.79688, -0.866007328, 0, 0.500031352, 0, 1, 0, -0.500031352, 0, -0.866007328)
+
+local FountainCity = Instance.new("Part", IslandPositions)
+FountainCity.Name = "FountainCity"
+FountainCity.Anchored = true
+FountainCity.CanCollide = false
+FountainCity.Transparency = 1
+FountainCity.CFrame = CFrame.new(5091.09619, 1.54258418, 4033.57031, 0.82929945, 2.17643645e-08, 0.558804452, -5.81348469e-10, 1, -3.80853287e-08, -0.558804452, 3.12592832e-08, 0.82929945)
+
+local Jungle = Instance.new("Part", IslandPositions)
+Jungle.Name = "Jungle"
+Jungle.Anchored = true
+Jungle.CanCollide = false
+Jungle.Transparency = 1
+Jungle.CFrame = CFrame.new(-1352.71985, 11.8931961, -117.559868, 0.791431606, 7.32992378e-09, 0.611257732, 1.99811332e-08, 1, -3.78623e-08, -0.611257732, 4.21790425e-08, 0.791431606)
+
+local MarineFortress = Instance.new("Part", IslandPositions)
+MarineFortress.Name = "MarineFortress"
+MarineFortress.Anchored = true
+MarineFortress.CanCollide = false
+MarineFortress.Transparency = 1
+MarineFortress.CFrame = CFrame.new(-4697.396, 20.6937809, 3974.98389, -0.0254837889, 6.01778094e-09, 0.999675214, 1.30715305e-09, 1, -5.98641403e-09, -0.999675214, 1.15417209e-09, -0.0254837889)
+
+local MiddleTown = Instance.new("Part", IslandPositions)
+MiddleTown.Name = "MiddleTown"
+MiddleTown.Anchored = true
+MiddleTown.CanCollide = false
+MiddleTown.Transparency = 1
+MiddleTown.CFrame = CFrame.new(-707.096313, 7.89353943, 1577.15942, -0.448824108, -1.30878959e-07, 0.893620133, -2.27290951e-08, 1, 1.35043507e-07, -0.893620133, 4.02996072e-08, -0.448824108)
+
+local PirateVillage = Instance.new("Part", IslandPositions)
+PirateVillage.Name = "PirateVillage"
+PirateVillage.Anchored = true
+PirateVillage.CanCollide = false
+PirateVillage.Transparency = 1
+PirateVillage.CFrame = CFrame.new(-1068.46948, 13.7933388, 3840.177, 0.258864343, 0, 0.965913713, 0, 1, 0, -0.965913713, 0, 0.258864343)
+
+local Prison = Instance.new("Part", IslandPositions)
+Prison.Name = "Prison"
+Prison.Anchored = true
+Prison.CanCollide = false
+Prison.Transparency = 1
+Prison.CFrame = CFrame.new(4860.41699, 5.69331741, 725.044067, -0.406715393, 0, 0.913554907, 0, 1, 0, -0.913554907, 0, -0.406715393)
+
+local UpperSky = Instance.new("Part", IslandPositions)
+UpperSky.Name = "UpperSky"
+UpperSky.Anchored = true
+UpperSky.CanCollide = false
+UpperSky.Transparency = 1
+UpperSky.CFrame = CFrame.new(-7946.89844, 5545.53418, -318.856445, 0.998631775, 0, -0.0522932447, 0, 1, 0, 0.0522932447, 0, 0.998631775)
+
+local Sky = Instance.new("Part", IslandPositions)
+Sky.Name = "Sky"
+Sky.Anchored = true
+Sky.CanCollide = false
+Sky.Transparency = 1
+Sky.CFrame = CFrame.new(-4823.28711, 717.702515, -2641.0769, -0.702142298, -9.33244095e-08, 0.712036669, -1.03479124e-07, 1, 2.90256672e-08, -0.712036669, -5.33007842e-08, -0.702142298)
+
+local FrozenVillage = Instance.new("Part", IslandPositions)
+FrozenVillage.Name = "FrozenVillage"
+FrozenVillage.Anchored = true
+FrozenVillage.CanCollide = false
+FrozenVillage.Transparency = 1
+FrozenVillage.CFrame = CFrame.new(1185.63379, 7.34473419, -1143.36987, -0.484826177, 0, 0.874610543, 0, 1, 0, -0.874610543, 0, -0.484826177)
+
+local PirateStarter = Instance.new("Part", IslandPositions)
+PirateStarter.Name = "PirateStarter"
+PirateStarter.Anchored = true
+PirateStarter.CanCollide = false
+PirateStarter.Transparency = 1
+PirateStarter.CFrame = CFrame.new(1159.87524, 16.5706196, 1346.3208, -0.75470823, 0, 0.656060576, 0, 1, 0, -0.656060576, 0, -0.75470823)
+
+local MarineStarter = Instance.new("Part", IslandPositions)
+MarineStarter.Name = "MarineStarter"
+MarineStarter.Anchored = true
+MarineStarter.CanCollide = false
+MarineStarter.Transparency = 1
+MarineStarter.CFrame = CFrame.new(-2566.01587, 6.89696407, 2047.82239, -0.267671078, -8.00114393e-08, 0.963510334, -1.36718974e-08, 1, 7.92434349e-08, -0.963510334, 8.03816125e-09, -0.267671078)
+
+local UnderwaterCity = Instance.new("Part", IslandPositions)
+UnderwaterCity.Name = "UnderwaterCity"
+UnderwaterCity.Anchored = true
+UnderwaterCity.CanCollide = false
+UnderwaterCity.Transparency = 1
+UnderwaterCity.CFrame = CFrame.new(3891.70215, 5.41444302, -1889.20227, -0.560523152, -3.98416447e-08, -0.828138769, -4.17776258e-09, 1, -4.52821602e-08, 0.828138769, -2.19219327e-08, -0.560523152)
+
+local WhirlpoolEnter = Instance.new("Part", IslandPositions)
+WhirlpoolEnter.Name = "WhirlpoolEnter"
+WhirlpoolEnter.Anchored = true
+WhirlpoolEnter.CanCollide = false
+WhirlpoolEnter.Transparency = 1
+WhirlpoolEnter.CFrame = CFrame.new(4038.5603, 1.24352789, -1813.67896, -0.565773964, 7.90609462e-08, -0.824560404, 1.25741184e-09, 1, 9.50197645e-08, 0.824560404, 5.27228963e-08, -0.565773964)
+
+local WhirlpoolExit = Instance.new("Part", IslandPositions)
+WhirlpoolExit.Name = "WhirlpoolExit"
+WhirlpoolExit.Anchored = true
+WhirlpoolExit.CanCollide = false
+WhirlpoolExit.Transparency = 1
+WhirlpoolExit.CFrame = CFrame.new(61167.0508, 1.33905935, 1956.63159, -0.995719731, 1.40456846e-08, -0.0924242139, 8.56154081e-09, 1, 5.97331606e-08, 0.0924242139, 5.86861901e-08, -0.995719731)
+
+local MagmaVillage = Instance.new("Part", IslandPositions)
+MagmaVillage.Name = "MagmaVillage"
+MagmaVillage.Anchored = true
+MagmaVillage.CanCollide = false
+MagmaVillage.Transparency = 1
+MagmaVillage.CFrame = CFrame.new(-5193.32373, 8.63197041, 8569.5498, 0.950981796, -1.61564166e-08, 0.309246838, 2.01535393e-08, 1, -9.73084635e-09, -0.309246838, 1.54862771e-08, 0.950981796)
