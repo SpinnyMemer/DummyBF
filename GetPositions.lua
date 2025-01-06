c4e873339b401d02ca2cb9143b68164b7fc1f1f2
SpinnyMemer <110652422+SpinnyMemer@users.noreply.github.com>
2025-01-06 21:10:30 +0530

    Update GetPositions.lua

--- GetPositions.lua
+++ GetPositions.lua
@@ -1,34 +1,37 @@
 local Dummyspace = game.Workspace:WaitForChild("Dummyspace")
 
-local Bandit = Instance.new("Part", Dummyspace)
+local FarmPositions = Instance.new("Folder", Dummyspace)
+FarmPositions.Name = "FarmPositions"
+
+local Bandit = Instance.new("Part", FarmPositions)
 Bandit.Name = "Bandit"
 Bandit.Anchored = true
 Bandit.CanCollide = false
 Bandit.Transparency = 1
 Bandit.CFrame = CFrame.new(1037.30884, 80.7064667, 1594.44678, 0.951819658, 2.68965348e-08, -0.306658387, -4.92265677e-08, 1, -6.50831069e-08, 0.306658387, 7.70431186e-08, 0.951819658)
 
-local JungleMonkey1 = Instance.new("Part", Dummyspace)
+local JungleMonkey1 = Instance.new("Part", FarmPositions)
 JungleMonkey1.Name = "JungleMonkey1"
 JungleMonkey1.Anchored = true
 JungleMonkey1.CanCollide = false
 JungleMonkey1.Transparency = 1
 JungleMonkey1.CFrame = CFrame.new(-1439.85718, 82.9646759, 239.7771, 0.389061451, -2.1013097e-08, 0.921211839, -5.64358871e-08, 1, 4.66452157e-08, -0.921211839, -7.01372613e-08, 0.389061451)
 
-local JungleMonkey2 = Instance.new("Part", Dummyspace)
+local JungleMonkey2 = Instance.new("Part", FarmPositions)
 JungleMonkey2.Name = "JungleMonkey2"
 JungleMonkey2.Anchored = true
 JungleMonkey2.CanCollide = false
 JungleMonkey2.Transparency = 1
 JungleMonkey2.CFrame = CFrame.new(-1759.22559, 59.8653336, 63.1509399, -0.412369341, 1.02978124e-08, -0.911016762, -5.65540592e-09, 1, 1.3863553e-08, 0.911016762, 1.08690736e-08, -0.412369341)
 
-local JungleGorilla = Instance.new("Part", Dummyspace)
+local JungleGorilla = Instance.new("Part", FarmPositions)
 JungleGorilla.Name = "JungleGorilla"
 JungleGorilla.Anchored = true
 JungleGorilla.CanCollide = false
 JungleGorilla.Transparency = 1
 JungleGorilla.CFrame = CFrame.new(-1265.76709, 66.2191696, -531.085999, 0.0876257941, -2.94118223e-08, -0.996153474, 1.96005292e-08, 1, -2.78012475e-08, 0.996153474, -1.70890289e-08, 0.0876257941)
 
---[[ local Template = Instance.new("Part", Dummyspace)
+--[[ local Template = Instance.new("Part", FarmPositions)
 Template.Name = "Template"
 Template.Anchored = true
 Template.CanCollide = false
